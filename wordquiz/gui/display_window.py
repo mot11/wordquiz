a8cf7c1b776321398bd95d9d44f0d1ee91fc4759
Satoshi Tohdas <moshi811@gmail.com>
2021-07-01 23:56:27 +0900

    Add rudimentary GUI

---
+++ wordquiz/gui/display_window.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+class DisplayWindow:
+
+    def __init__(self):
+        self.window = tk.Tk()
+
+    def open_window(self):
+        self.window.mainloop()
+
---
+++ wordquiz/runner.py
@@ -0,0 +1,10 @@
+from gui.display_window import DisplayWindow
+
+
+def main():
+    dw = DisplayWindow()
+    dw.open_window()
+
+
+if __name__ == "__main__":
+    main()
